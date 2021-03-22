84cc0d14d510f7d4eb7ebfcef9495799114fb408
zuble <rbfeel1@gmail.com>
2021-03-22 19:59:02 +0000

    insere + reordena~ + mods to planta/colecao noca

--- plantas.c
+++ plantas.c
@@ -7,17 +7,47 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+void imprime(colecao * c){
+	for (int i = 0; i < c->tamanho; i++){
+		printf("plantas [%d] ~~ nome : %s | id : %s | nºalcunhas : %d | nºsementes : %d \n" , i , c->plantas[i]->nome_cientifico , c->plantas[i]->ID , c->plantas[i]->n_alcunhas , c->plantas[i]->n_sementes);
+	}
+	puts("");
+	/*
+	puts("alcunhas");
+	for( int k = 0 ; k < c->tamanho ; k++){
+		printf("%s : " , c->plantas[k]->nome_cientifico);
+		for (int i = 0; i < c->plantas[k]->n_alcunhas; i++){
+			printf("%s |", c->plantas[k]->alcunhas[i]);
+		}
+		puts("");
+	}
+	puts("");
+	*/
+}
+
 planta *planta_nova(const char *ID, const char *nome_cientifico, char **alcunhas, int n_alcunhas, int n_sementes){
-	
-	if( ID == NULL || nome_cientifico == NULL || alcunhas == NULL || n_alcunhas < 0 || n_sementes < 0) return NULL;
+
+	if( ID == NULL || sizeof(ID) > 10 || nome_cientifico == NULL || sizeof(nome_cientifico) > MAX_NAME|| n_alcunhas < 0 || n_sementes < 0) return NULL;
 
 	planta* nova = (planta*) malloc(sizeof(planta));
+	if(!nova) return NULL;
 
 	strncpy( nova->ID , ID , 10 );
 	strncpy( nova->nome_cientifico , nome_cientifico , MAX_NAME);
-	nova->alcunhas = alcunhas;
-	nova->n_alcunhas = n_alcunhas;
 	nova->n_sementes = n_sementes;
+	nova->n_alcunhas = n_alcunhas;
+
+	if ( n_alcunhas > 0 ){
+		nova->alcunhas = (char**) calloc( n_alcunhas, sizeof(char*));
+		if(!nova->alcunhas) return NULL;
+
+		for ( int  i = 0; i < n_alcunhas; i++){
+
+			nova->alcunhas[i]= (char *)malloc(sizeof(char) * strlen(alcunhas[i]+1));
+            strcpy(nova->alcunhas[i], alcunhas[i]);
+
+		}
+	}
 
 	return nova;
 }
@@ -31,79 +61,58 @@ colecao *colecao_nova(const char *tipo_ordem){
 
 	nova->tamanho = 0;
 	strncpy( nova->tipo_ordem , tipo_ordem , 5 );
+	//nova->plantas = (planta**) malloc( sizeof(planta*));
 	nova->plantas = NULL;
-
 	return nova;
 }
 
 int planta_insere(colecao *c, planta *p){
-	
-	if(c == NULL || p == NULL ) return -1;
 
-	planta * aux;
-	long int i ;
-	int aux_alcunha = 0 ;
+		if(p == NULL ) return -1;
 
-	printf("%ld" , c->tamanho);
-	//************************************************************************************
-	if (c->tamanho  != 0 ){
-		puts("ola");
-		for (i = 0; i < c->tamanho; i++){
-			
-			//existe planta com mesmo ID 
+		planta * aux1 , * aux2;
+		int aux_alcunha = 0;
+
+		for (int i = 0; i < c->tamanho; i++){
+
+			//caso exista planta com mesmo ID que p
 			if ( strcmp( c->plantas[i]->ID , p->ID ) == 0 ) {
-				
+
 				//atualiza nº sementes
-				c->plantas[i]->n_sementes =+ p->n_sementes;
-				int n_alcunha_c = c->plantas[i]->n_alcunhas;
-				int n_alcunha_p = p->n_alcunhas;
-				
+				c->plantas[i]->n_sementes = c->plantas[i]->n_sementes + p->n_sementes;
+
 				//atualiza alcunhas
 				for ( int k = 0; k < p->n_alcunhas ; k++){
-					for (int m = 0; m < n_alcunha_c; m++){
-						if( strcmp( p->alcunhas[k] , c->plantas[i]->alcunhas[m] ) == 0){
-							aux_alcunha = 1; //alcunha k já existe
-						}
+					for (int m = 0; m < c->plantas[i]->n_alcunhas ; m++){
+
+						//alcunha p->alcunhas[k] ja existe na colecao
+						if( strcmp( p->alcunhas[k] , c->plantas[i]->alcunhas[m] ) == 0) aux_alcunha = 1;
+
 					}
+
 					if( aux_alcunha == 0){
-						c->plantas[i]->alcunhas[n_alcunha_c] = p->alcunhas[k];
+						c->plantas[i]->n_alcunhas = c->plantas[i]->n_alcunhas + 1;
+						c->plantas[i]->alcunhas = (char**) realloc( c->plantas[i]->alcunhas , c->plantas[i]->n_alcunhas * sizeof(char*) );
+						c->plantas[i]->alcunhas[ c->plantas[i]->n_alcunhas - 1] = p->alcunhas[k];
 					}
-					aux_alcunha = 0;				
+					aux_alcunha = 0;
 				}
-			
-				return 1;			
+				return 1;
 			}
-
-			if ( strcmp(c->tipo_ordem , "nome") == 0 ) {
-				puts("poal");			
-			}
-
-			if (  strcmp(c->tipo_ordem , "id") == 0 ) {
-				puts("poal");			
-			}
-
 		}
 
-	}
+		//insere no fim do vetor plantas
+		c->tamanho++;
+		c->plantas = (planta**)realloc(c->plantas , c->tamanho * sizeof(planta));
+		c->plantas[c->tamanho-1] = p;
+		//reordena
+		colecao_reordena(c, c->tipo_ordem);
 
-	else{
-		puts("ola");
-		
-		//strncpy( c->plantas[0]->ID , p->ID , 10 );
-		//strncpy( c->plantas[0]->nome_cientifico , p->nome_cientifico , MAX_NAME);
-		//c->plantas[0]->alcunhas = p->alcunhas;
-		//c->plantas[0]->n_alcunhas = p->n_alcunhas;
-		//c->plantas[0]->n_sementes = p->n_sementes;
-		
-		c->plantas[0] = p ;
 		return 0;
-	}
-//********************************************************************************************
-	return -1;
 }
+
 int colecao_tamanho(colecao *c){
 	if(c == NULL) return -1;
-
 
 	return c->tamanho;
 }
@@ -152,7 +161,7 @@ int *colecao_pesquisa_nome(colecao *c, const char *nomep, int *tam){
 				m == n_alcunha_c;
 			}
 		}
-	
+
 		if (achou==1)
 		{
 			indi[cont] = c->plantas[i]->ID;
@@ -167,9 +176,56 @@ int *colecao_pesquisa_nome(colecao *c, const char *nomep, int *tam){
 	else
 		return NULL;
 
-	
+
 }
 
 int colecao_reordena(colecao *c, const char *tipo_ordem){
-	return -1;
+
+	if( c == NULL || tipo_ordem == NULL ) return -1;
+
+	planta * aux;
+	int ordem = 0;
+
+	if( strcmp( "nome" , tipo_ordem ) == 0 ){
+
+		for (int i = 0; i < c->tamanho; i++){
+			for (int k = i + 1; k < c->tamanho; k++){
+
+				if ( strcmp(c->plantas[i]->nome_cientifico , c->plantas[k]->nome_cientifico ) > 0) {
+					ordem = 1;
+					aux = c->plantas[i];
+					c->plantas[i] = c->plantas[k];
+					c->plantas[k] = aux;
+					//imprime(c);
+				}
+
+			}
+
+		}
+		strcpy(c->tipo_ordem , tipo_ordem);
+		if( ordem == 1 ) return 1;
+		if( ordem == 0 ) return 0;
+	}
+
+	if( strcmp( "id" , tipo_ordem ) == 0 ){
+
+		for (int i = 0; i < c->tamanho; i++){
+			for (int k = i + 1; k < c->tamanho; k++){
+
+				if ( strcmp(c->plantas[i]->ID , c->plantas[k]->ID ) > 0) {
+					ordem = 1;
+					aux = c->plantas[i];
+					c->plantas[i] = c->plantas[k];
+					c->plantas[k] = aux;
+					//imprime(c);
+				}
+
+			}
+
+		}
+		strcpy(c->tipo_ordem , tipo_ordem);
+		if( ordem == 1 ) return 1;
+		if( ordem == 0 ) return 0;
+	}
+
 }
